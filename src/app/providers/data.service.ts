49e5fca5e3ac0e517b2fbc069c8883b95a22863b
Steevens98 <67281150+Steevens98@users.noreply.github.com>
2023-09-01 22:39:00 -0500

    endpoints

--- src/app/providers/data.service.ts
+++ src/app/providers/data.service.ts
@@ -7,9 +7,9 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private http:HttpClient) { }
-  private URL: string = 'http://localhost:3000/rest/Paciente/findAll/json';
-  private URLMedico: string = 'http://localhost:3000/rest/Medico/findAll/json';
-  private URLEspecialidad: string = 'http://localhost:3000/rest/Especialidad/findAll/json'
+  private URL: string = 'https://proyecto7express-production.up.railway.app/rest/Paciente/findAll/json';
+  private URLMedico: string = 'https://proyecto7express-production.up.railway.app/rest/Medico/findAll/json';
+  private URLEspecialidad: string = 'https://proyecto7express-production.up.railway.app/rest/Especialidad/findAll/json'
 
   
   getResponse() {
